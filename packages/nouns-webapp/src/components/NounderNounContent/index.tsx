12b7df90b094360767ef3deb5a7c1cdc8b647d36
Willy Ogorzaly <willy@ogorzaly.com>
2024-01-30 13:28:44 -0600

    Attempt #2 at fixing ATX DAO Treasury URL
    last attempt fixed it if the hyperlink was opened in a new tab, but not if the hyperlink was clicked regularly. 
    
    this should fix for good

--- packages/nouns-webapp/src/components/NounderNounContent/index.tsx
+++ packages/nouns-webapp/src/components/NounderNounContent/index.tsx
@@ -101,9 +101,9 @@ const NounderNounContent: React.FC<{
               }
             >
               <Trans>Every 10th ATX Noun is transferred to the </Trans>{' '}
-              <Link to={{ pathname: "https://etherscan.io/tokenholdings", search: "?a=0x407Cf0e5Dd3C2c4bCE5a32B92109c2c6f7f1ce23" }} className={nounContentClasses.link}>
-                <Trans>ATX DAO Treasury</Trans>
-              </Link>
+              <a href="https://etherscan.io/tokenholdings?a=0x407Cf0e5Dd3C2c4bCE5a32B92109c2c6f7f1ce23" className={nounContentClasses.link}>
+              {ATX DAO Treasuy}
+              </a>
               .{' '}
               <Trans>
                 This means that the future of these ATX Nouns is up to the community to decide. 
